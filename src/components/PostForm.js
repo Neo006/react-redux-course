44627027c0af2f88645975d07a2d9f22a2770c0d
HaykBarca <hayk19939563@gmail.com>
2020-09-29 00:35:47 +0400

    Development

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,8 +1,35 @@
 import React from "react";
+import {connect} from "react-redux";
+import {createPost} from "../redux/actions";
 
-export default class PostForm extends React.Component {
+ class PostForm extends React.Component {
+    state = {
+        title: ''
+    }
+
     submitHandler = event => {
-        event.preventDefault()
+        event.preventDefault();
+
+        let {title} = this.state;
+        if (!title.trim()) {
+            return
+        }
+
+        let newPost = {
+            title,
+            id: Date.now().toString()
+        }
+
+        this.props.createPost(newPost);
+        this.setState({title: ''});
+    }
+
+    changeInputHandler = event => {
+        event.persist();
+        this.setState(prev => ({...prev, ...{
+                [event.target.name]: event.target.value
+            }})
+        )
     }
 
     render() {
@@ -10,10 +37,22 @@ export default class PostForm extends React.Component {
             <form onSubmit={this.submitHandler} className="mt-3 mb-4">
                 <div className="form-group">
                     <label htmlFor="title">Post title</label>
-                    <input type="text" className="form-control" id="title" />
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        value={this.state.title}
+                        name="title"
+                        onChange={this.changeInputHandler} />
                 </div>
                 <button className="btn btn-success" type="submit">Create</button>
             </form>
         )
     }
 }
+
+const mapDispatchToProps = {
+     createPost
+}
+
+export default connect(null, mapDispatchToProps)(PostForm)
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Post from "./Post";
+import {connect} from "react-redux";
 
-export default ({posts}) => {
-    if (!posts.length) {
+const Posts = ({syncPosts}) => {
+    if (!syncPosts.length) {
         return <p>No posts yet</p>
     }
-    return posts.map(post => <Post post={post} key={post} />)
+    return syncPosts.map(post => <Post post={post} key={post.id} />)
 }
+
+const mapStateToProps = state => {
+    return {
+        syncPosts: state.postsReducer.posts
+    }
+}
+
+export default connect(mapStateToProps, null)(Posts);
