9b8db46b7b922ffb6cbfef78d7cce5f5d77c9c77
HaykBarca <hayk19939563@gmail.com>
2020-09-28 18:07:38 +0400

    Development

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {compose, createStore} from "redux";
+import {Provider} from "react-redux";
+import {rootReducer} from "./redux/rootReducer";
+
+const store = createStore(rootReducer, compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+const app = (
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
